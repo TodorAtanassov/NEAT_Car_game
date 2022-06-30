28d6512709c1669ea2514dc56af5c712b0277e78
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-30 12:30:58 +0300

    Core

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ height = 1080
 car_size_x = 60
 car_size_y = 60
 
-border_color = (255, 255, 255, 255) #Color in case of crash
+border_color = (255, 255, 255, 255)  # Color in case of crash
 
-current_generation = 0 #Generation counter
+current_generation = 0  # Generation counter
