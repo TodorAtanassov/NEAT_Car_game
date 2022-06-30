48d49d0ae6116068684ef074f7fd1e244cffec76
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-30 12:25:23 +0300

    Core

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import os
 import neat
 import pygame
 
+width = 1920
+height = 1080
+
+car_size_x = 60
+car_size_y = 60
+
+border_color = (255, 255, 255, 255) #Color in case of crash
+
+current_generation = 0 #Generation counter
